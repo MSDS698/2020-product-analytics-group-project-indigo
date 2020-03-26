eb758d5751fee1ab87d8ea1744f014972b9cd16a
jgoffin <jacobgoffin@gmail.com>
2020-03-25 18:51:33 -0700

    .

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 echo -e "3\nIndigo\nY\n1\nn\nn" | eb init -i --profile pa_iam
 eb create indigo-webserver
-aws codepipeline create-pipeline --cli-input-json file://pipeline.json
+aws codepipeline create-pipeline --cli-input-json file://pipeline.json --profile pa_iam
