95ef35d77f4e0776e323105f2a2800c6ed60bfa9
jgoffin <jacobgoffin@gmail.com>
2020-03-25 23:41:15 -0700

    removed --profile option from deploy

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
-echo -e "3\nIndigo\nY\n1\nn\nn" | eb init -i --profile pa_iam
+echo -e "3\nIndigo\nY\n1\nn\nn" | eb init -i
 eb create indigo-webserver
-aws codepipeline create-pipeline --cli-input-json file://pipeline.json --profile pa_iam
+aws codepipeline create-pipeline --cli-input-json file://pipeline.json
