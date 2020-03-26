2b7780a0c62f462fa1e05149802aceb651cb8ba3
jgoffin <jacobgoffin@gmail.com>
2020-03-25 17:59:44 -0700

    adding flask code

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
-echo -e "3\nIndigoApp\nY\n1\nn\nn" | eb init -i
-eb create indigo-app
-aws codepipeline create-pipeline --cli-input-json file://pipeline.json --profile pa_iam
+echo -e "3\nIndigo\nY\n1\nn\nn" | eb init -i --profile pa_iam
+eb create indigo-webserver
+aws codepipeline create-pipeline --cli-input-json file://pipeline.json
