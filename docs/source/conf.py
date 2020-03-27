095ba821fb05d0096842535abcd0992a995403bc
Alan Flint <alan@flintagan.org>
2020-03-26 19:35:05 -0700

    Updated Sphinx with source link to code

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,7 +31,13 @@ release = '0'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-'sphinx.ext.autodoc'
+'sphinx.ext.autodoc',
+'sphinx.ext.doctest',
+'sphinx.ext.intersphinx',
+'sphinx.ext.todo',
+'sphinx.ext.coverage',
+'sphinx.ext.viewcode',
+'sphinx.ext.githubpages'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
