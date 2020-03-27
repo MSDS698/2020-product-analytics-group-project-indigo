75ad9f180b85732ea6e33b916b50038be13a0585
jgoffin <jacobgoffin@gmail.com>
2020-03-26 20:57:13 -0700

    adding demo page to show Magenta functionality

--- application.py
+++ application.py
@@ -74,6 +74,12 @@ def upload():
     return render_template('upload.html', form=file)
 
 
+@application.route('/demo', methods=['GET', 'POST'])
+def demo():
+    """ Load demo page showing Magenta """
+    return render_template('demo.html')
+
+
 if __name__ == '__main__':
     application.jinja_env.auto_reload = True
     application.config['TEMPLATES_AUTO_RELOAD'] = True
