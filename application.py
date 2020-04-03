f0d3bcc7644e043c42f1bc745b8185d0c8448432
hannahlyon <hbylyon1@gmail.com>
2020-04-02 17:55:25 -0700

    added register

--- application.py
+++ application.py
@@ -7,6 +7,7 @@ import csv
 import os
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
+from classes import * 
 
 
 # Initialization
@@ -51,17 +52,31 @@ def register():
     """
     Register a new user. Save the username, password and email.
     """
-    form = RegistrationForm(request.form)
-    if request.method == 'POST' and form.validate():
-        # user = User(form.username.data, form.email.data,
-        #             form.password.data)
-        with open('users.csv', mode='w+', newline='') as accounts_file:
-            accounts_writer = csv.writer(accounts_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-            accounts_writer.writerow([form.username.data, form.email.data, form.password.data])
-        # db_session.add(user)
-        flash('Thanks for registering')
-        return redirect(url_for('index'))
-    return render_template('register.html', form=form)
+#    form = RegistrationForm(request.form)
+#    if request.method == 'POST' and form.validate():
+#        # user = User(form.username.data, form.email.data,
+#        #             form.password.data)
+#        with open('users.csv', mode='w+', newline='') as accounts_file:
+#            accounts_writer = csv.writer(accounts_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+#            accounts_writer.writerow([form.username.data, form.email.data, form.password.data])
+#        # db_session.add(user)
+#        flash('Thanks for registering')
+#        return redirect(url_for('index'))
+#    return render_template('register.html', form=form)
+    registration_form = classes.RegistrationForm()
+    if registration_form.validate_on_submit():
+        username = registration_form.username.data
+        password = registration_form.password.data
+        email = registration_form.email.data
+        user_cnt = User.query.filter_by(username=username).count() + User.query.filter_by(email=email).count()
+        if user_cnt == 0:
+            user = User(username, email, password)
+            db.session.add(user)
+            db.session.commit()
+            return redirect(url_for('music'))
+        else:
+            return '<p> Error- Existing User </p>'
+    return render_template(register.html, form=regration_form )
 
 
 @application.route('/upload', methods=['GET', 'POST'])
