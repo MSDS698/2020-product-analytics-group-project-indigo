4e0e02bb6aa44eba46f75a03f689d3b6f56b4cd5
jgoffin <jacobgoffin@gmail.com>
2020-04-06 20:01:16 -0700

    refactored code (declare variables), added comments

--- static/midi_reader.js
+++ static/midi_reader.js
@@ -1,10 +1,33 @@
+// Array to hold PianoRollSVGVisualizer objects (one for each track)
+let noteCanvases = [];
+// Array to hold music players (one for each track) (Not used yet, currently still just using one main player)
+let players = [];
+
+// Original Complete Midi File
+const main_player = initialize_player();
+
+//note sequences
+let note_seq;
+let quant_note_seq;
+let separated_sequences;
+
+// programs in midi file
+let programs;
+let program_notes;
+
+// models
+let music_rnn;
+let music_vae;
+
 function initialize_player(){
+  // Use soundfount to be able to play different instruments (not just piano)
   const player = new mm.SoundFontPlayer('https://storage.googleapis.com/magentadata/js/soundfonts/sgm_plus');
   return player
 
 }
 
 // Given note-sequence for entire MIDI file, filter to just notes for one program (instrument)
+// Part of functionality to split out original MIDI file into separate instruments
 function notes_filter(program){
   filtered_notes =  quant_note_seq['notes'].filter(
     function (note) {
@@ -20,6 +43,7 @@ function notes_filter(program){
 }
 
 // Replace notes of a given sequence with new value, tag returned object with the name of the instrument
+// Basically recreates the original note_sequence object, but just with the notes from a single program/instrument
 function replace_notes_in_sequence(sequence, obj){
   seq_clone = mm.sequences.clone(sequence)
   seq_clone['notes'] = obj["notes"]
@@ -30,8 +54,17 @@ function replace_notes_in_sequence(sequence, obj){
 // Format element to be appended
 function format_track(sequence, index, array){
   $("#tracks").append("<h3>"+sequence['instrument']+"</h3>");
-  $("#tracks").append("<input type='image' src='static/playbutton.png' height='30px' width='30px' onclick='player.start(separated_sequences["+index+"][\"trimmed_sequence\"])'/>");
-  $("#tracks").append("<input type='image' src='static/stopbutton.jpeg' height='30px' width='30px' onclick='player.stop()");
+  // Add new player to control this track (mainly so callback/draw can be controlled on individual tracks). Not yet in use
+  players.push(initialize_player);
+  $("#tracks").append("<input type='image' src='static/playbutton.png' height='30px' width='30px' onclick='main_player.start(separated_sequences["+index+"][\"trimmed_sequence\"])'/>");
+  $("#tracks").append("<input type='image' src='static/stopbutton.png' height='30px' width='30px' onclick='main_player.stop()'/>");
+  // SVG element holds the visualized note image
+  $("#tracks").append("<div style='overflow:scroll; width:800px'><svg id='noteCanvas"+index+"'></svg></div>");
+
+  noteCanvases.push(new mm.PianoRollSVGVisualizer(
+    separated_sequences[index]['trimmed_sequence'], document.getElementById('noteCanvas'+index), 
+    {noteRGB:'35,70,90', activeNoteRGB:'157, 229, 184', noteHeight:5}));
+
 }
 
 // Create UI for separate tracks
@@ -39,7 +72,7 @@ function add_tracks(sequences){
   sequences.forEach(format_track);
 }
 
-//
+// Main function, kicks everything else off
 function load_midi_sample(){
   // Load midi from url and chain operations to create note-sequence, quantize, and show play button
   mm.urlToNoteSequence('https://bitmidi.com/uploads/17583.mid')
@@ -47,10 +80,11 @@ function load_midi_sample(){
             .then(unimportant => quant_note_seq = mm.sequences.quantizeNoteSequence(note_seq, 4))
             .then(function(){$("#playbutton").show()})
             .then(function(){
-              const programs = [...new Set(quant_note_seq['notes'].map(note => note.program))];
-  
+              // Find the programs/instruments that exist in the MIDI file
+              programs = [...new Set(quant_note_seq['notes'].map(note => note.program))];
+              // Create an array of arrays, one array for each program/instrument's notes
               program_notes = programs.map(program => notes_filter(program));
-
+              // Create array of Magenta note_sequence objects, one fore each program/instrument
               separated_sequences =  program_notes.map(obj => replace_notes_in_sequence(quant_note_seq, obj));
 
 
@@ -59,7 +93,8 @@ function load_midi_sample(){
               // call function to load UI showing each track separately
               add_tracks(separated_sequences);
             })          
-  
+
+  // Load models
   music_rnn = new mm.MusicRNN('https://storage.googleapis.com/magentadata/js/checkpoints/music_rnn/melody_rnn');
   // Only give users ability to run sample through model AFTER model has finished initializing
   music_rnn.initialize()
@@ -70,18 +105,18 @@ function load_midi_sample(){
 
 }
 
-// try using mostly jquery to handle DOM manipulation
+// Event Listeners
 $("#loadmidi").click(function(){
   load_midi_sample();
 });
 
 $("#playbutton").click(function(){
-  player.start(note_seq);
+  main_player.start(note_seq);
   $("#stopbutton").show()
 });
 
 $("#stopbutton").click(function(){
-  player.stop()
+  main_player.stop()
 });
 
 $("#rnnsample").click(function(){
@@ -90,7 +125,7 @@ $("#rnnsample").click(function(){
 
   let trim_note_seq = mm.sequences.trim(quant_note_seq, 0, 300);
   music_rnn.continueSequence(trim_note_seq, rnn_steps, rnn_temp)
-    .then(sample => player.start(sample));
+    .then(sample => main_player.start(sample));
 });
 
 $("#vaesample").click(function(){
@@ -98,10 +133,7 @@ $("#vaesample").click(function(){
   
   let trim_note_seq = mm.sequences.trim(quant_note_seq, 0, 300);
   music_vae.sample(1, vae_temperature)
-    .then(sample => player.start(sample[0]));
+    .then(sample => main_player.start(sample[0]));
 });
 
 
-// First thing page does is intialize player, but how do I know player is ready when user clicks button?
-// This seems janky and like there should be a better way to start player
-const player = initialize_player()
