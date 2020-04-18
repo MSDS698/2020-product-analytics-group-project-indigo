184a8c967db9912ae8b87c1e08011ff58b6158a2
Alan Flint <alan@flintagan.org>
2020-04-18 16:45:54 -0700

    Test user upload, more robust testing framework

--- test/test_cases.py
+++ test/test_cases.py
@@ -1,10 +1,25 @@
-import os
-import sys
+import boto3
 import pytest
+import io
+from application import application, db, Customer, Files
 
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-from application import db, Customer, application
+TEST_USER = dict(user='TEST_USER',
+                 email='test@gmail.com',
+                 password='testpass')
+
+
+@pytest.fixture(scope='module')
+def client():
+    application.config.update(TESTING=True,
+                              WTF_CSRF_ENABLED=False)
+    test_client = application.test_client()
+    yield test_client
+
+
+@pytest.fixture(scope='module')
+def init_database(client):
+    yield db
 
 
 def UserFromDB(username):
@@ -15,32 +30,12 @@ def UserFromDB(username):
     return user
 
 
-@pytest.fixture(scope='module')
-def client():
-    basedir = os.path.abspath(os.path.dirname(__file__))
-    application.config.update(
-        TESTING=True,
-        SQLALCHEMY_DATABASE_URI='sqlite:///' + os.path.join(basedir,
-                                                            'test.db'),
-        WTF_CSRF_ENABLED=False)
-
-    with application.test_client() as c:
-        yield c
-
-
-@pytest.fixture(scope='module')
-def init_database():
-    db.create_all()
-
-    user1 = Customer('post malone', 'post@gmail.com', 'watermalone')
-    user2 = Customer('tester', 'test@test.com', 'test123')
-    db.session.add(user1)
-    db.session.add(user2)
-    db.session.commit()
-
-    yield db
-
-    db.drop_all()
+def FileFromDB(username):
+    """
+    Function that returns a list of upload original file names for a given user
+    """
+    files = Files.query.filter_by(user_name=username).first()
+    return files
 
 
 def test_home(client):
@@ -72,17 +67,28 @@ def test_music_not_logged_in(client):
     assert response.status_code == 401
 
 
-def test_login_success(client, init_database):
-    response = client.post('/login', data=dict(username='post malone',
-                                               password='watermalone'),
+def test_register_test_user(client):
+    response = client.post('/register', data=dict(username=TEST_USER['user'],
+                                                  email=TEST_USER['email'],
+                                                  password=TEST_USER['password'],
+                                                  confirm=TEST_USER['password'],
+                                                  accept_tos='y'),
                            follow_redirects=True)
     assert response.status_code == 200
-    assert b'GET STARTED' in response.data
+    assert b'INDIGO- Login' in response.data
+
+
+def test_login_test_user(client, init_database):
+    response = client.post('/login', data=dict(username=TEST_USER['user'],
+                                               password=TEST_USER['password']),
+                           follow_redirects=True)
+    assert response.status_code == 200
+    assert b'INDIGO- Make Music' in response.data
     assert b'Login' not in response.data
 
 
 def test_login_username_failure(client, init_database):
-    response = client.post('/login', data=dict(username='test',
+    response = client.post('/login', data=dict(username='iklsjts',
                                                password='test123'),
                            follow_redirects=True)
     assert b'Login' in response.data
@@ -91,7 +97,7 @@ def test_login_username_failure(client, init_database):
 
 
 def test_login_password_failure(client, init_database):
-    response = client.post('/login', data=dict(username='post malone',
+    response = client.post('/login', data=dict(username=TEST_USER['user'],
                                                password='test123'),
                            follow_redirects=True)
     assert b'Login' in response.data
@@ -100,46 +106,70 @@ def test_login_password_failure(client, init_database):
 
 
 def test_login_unregistered(client, init_database):
+    # Need to implement this
     pass
 
 
 def register_duplicate_email(client, init_database):
-    response = client.post('register', data=dict(username='duplicate',
-                                                 email='post@gmail.com',
-                                                 password='123456',
-                                                 confirm='123456',
-                                                 accept_tos='y'),
+    response = client.post('/register', data=dict(username='duplicate',
+                                                  email='test@gmail.com',
+                                                  password='123456',
+                                                  confirm='123456',
+                                                  accept_tos='y'),
                            follow_redirects=True)
     assert b'Repeat Password' in response.data
 
 
 def register_duplicate_username(client, init_database):
-    response = client.post('register', data=dict(username='post malone',
-                                                 email='NOTpost@gmail.com',
-                                                 password='123456',
-                                                 confirm='123456',
-                                                 accept_tos='y'),
+    response = client.post('/register', data=dict(username=TEST_USER['user'],
+                                                  email='something@gmail.com',
+                                                  password='123456',
+                                                  confirm='123456',
+                                                  accept_tos='y'),
                            follow_redirects=True)
     assert b'Repeat Password' in response.data
 
 
-def test_create_user():
-    """
-    Function that creates a user and checks that the username, email, and
-    password have been stored correctly in the database
-    """
-    assert UserFromDB('post malone').username == 'post malone'
-    assert UserFromDB('post malone').email == 'post@gmail.com'
-    assert UserFromDB('post malone').check_password('watermalone')
-    assert UserFromDB('post malone').password_hash != 'watermalone'
+def test_file_upload(client, init_database):
+    client.post('/login', data=dict(username=TEST_USER['user'],
+                                    password=TEST_USER['password']),
+                follow_redirects=True)
+    try:
+        response = client.post('/upload', data=dict(file_selector=
+                                                    (io.BytesIO(b"test string"),
+                                                     'Queen_test.mid')),
+                               follow_redirects=True,
+                               content_type='multipart/form-data')
+        assert b'Select from uploads' in response.data
+        assert FileFromDB(TEST_USER['user']).orig_filename == 'Queen_test'
+
+        s3_file_name = FileFromDB(TEST_USER['user']).our_filename
+        s3 = boto3.resource('s3')
+        s3.Object('midi-file-upload', s3_file_name).delete()
+
+        Files.query.filter_by(user_name=TEST_USER['user']).delete()
+        db.session.commit()
+
+    except boto3.exceptions.S3UploadFailedError:
+        assert True
 
 
-def test_create_two_users():
-    """
-    Function that creates two users and checks that individual user details can
-    be correctly returned from the database
-    """
-    assert not UserFromDB('tester').username == 'post malone'
-    assert UserFromDB('tester').email == 'test@test.com'
-    assert not UserFromDB('tester').check_password('watermalone')
+def test_logout_logged_in_user(client, init_database):
+    client.post('/login', data=dict(username=TEST_USER['user'],
+                                    password=TEST_USER['password']),
+                follow_redirects=True)
+    response = client.get('/logout', follow_redirects=True)
+    assert response.status_code == 200
+    assert b'LOGIN' in response.data
 
+
+def test_logout_logged_out_user(client, init_database):
+    response = client.get('/logout')
+    # 401 is unauthorized status code
+    assert response.status_code == 401
+
+
+def test_remove_test_user(init_database):
+    Customer.query.filter_by(username=TEST_USER['user']).delete()
+    db.session.commit()
+    assert UserFromDB(TEST_USER['user']) is None
