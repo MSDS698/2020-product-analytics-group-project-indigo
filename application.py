0a9a31651dcf6852826edea953daa8fd116dc2c6
hannahlyon <hbylyon1@gmail.com>
2020-04-21 15:25:06 -0700

    added login to nav and error messages

--- application.py
+++ application.py
@@ -140,8 +140,7 @@ def register():
         user_count = Customer.query.filter_by(username=username).count() \
             + Customer.query.filter_by(email=email).count()
         if user_count > 0:
-            return '<h1>Error - Existing user : ' + username \
-                   + ' OR ' + email + '</h1>'
+            flash('Username or email already exists')
         else:
             user = Customer(username, email, password)
             db.session.add(user)
