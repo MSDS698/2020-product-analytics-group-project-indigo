cbefcf16a808c89259d81b41806d8231d343996f
ash-jha <ashwinijha28@gmail.com>
2020-05-05 17:48:00 -0700

    bless Jacob

--- static/script/vae.js
+++ static/script/vae.js
@@ -11,35 +11,32 @@ const vae_temperature = 1.5
 
 let upload1_quant;
 let upload2_quant;
+let note_seq1;
+let note_seq2;
 
 function interpolate(midi1, midi2) {
   
-  //Quantize for musicVAE model
-  const upload1 = (mm.urlToNoteSequence(midi1)
-                  .then(ns_val => note_seq1 = ns_val)
-                  .then(vars => upload1_quant = mm.sequences.quantizeNoteSequence(note_seq1, 4)))
-  const upload2 = (mm.urlToNoteSequence(midi2)
-                  .then(ns_val => note_seq2 = ns_val)
-                  .then(vars => upload2_quant = mm.sequences.quantizeNoteSequence(note_seq2, 4)))
-
-  // mm.Player.tone.context.resume();  // enable audio
-  music_vae
-  .interpolate([upload1_quant, upload2_quant], 4)
-  .then((sample) => {
-    const concatenated = mm.sequences.concatenate(sample);
-    vaePlayer.start(concatenated);
-  });
-
-  if (vaePlayer.isPlaying()) {
-
-    vaePlayer.stop();
-    return;
-  }
+  //Quantize note sequences and pass note sequences to interpolate function of MusicVAE
+  mm.urlToNoteSequence(midi1)
+                  .then(ns_val1 => note_seq1 = ns_val1)
+                  .then(vars => upload1_quant = mm.sequences.quantizeNoteSequence(note_seq1, 4))
+                  .then(vars => mm.urlToNoteSequence(midi2))
+                  .then(ns_val2 => note_seq2 = ns_val2)
+                  .then(vars => upload2_quant = mm.sequences.quantizeNoteSequence(note_seq2, 4))
+                  .then(vars => music_vae
+                      .interpolate([upload1_quant, upload2_quant], 4)
+                      .then((sample) => {
+                        const concatenated = mm.sequences.concatenate(sample);
+                        vaePlayer.start(concatenated);
+                      }));
 }
 
-function first(midi1, midi2){
-
-  interpolate(midi1, midi2);
-  setTimeout(() => {  interpolate(midi1, midi2); }, 2000);
-  
+function stop(){
+  if (vaePlayer.isPlaying()) {
+    vaePlayer.stop();
+  }
+  else{
+    vaePlayer.start();
+  }
+  return;
 }
