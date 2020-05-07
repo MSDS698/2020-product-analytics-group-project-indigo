39975397cc769d6f06b439523bcc3cbd7507b336
jgoffin <jacobgoffin@gmail.com>
2020-05-06 18:11:04 -0700

    download code

--- static/script/drums.js
+++ static/script/drums.js
@@ -38,6 +38,9 @@ let program_notes;
 // models
 let music_rnn;
 
+//download
+let output_midi;
+
 init();
 
 function init(){
@@ -185,6 +188,7 @@ $("#drums_rnn").click(function(){
         combined_seq['notes'].push(...generated_seq['notes']);
 
         $("#combined").show();
+        $("#download").show();
     })
 });
 
@@ -200,6 +204,26 @@ $("#stopCombined").click(function(){
 $("#btnPlaySample").click( (e) => play(samplePlayer, note_seq));
 $("#btnPlayDrums").click( (e) => play(drumsPlayer, generated_seq));
 $("#btnPlayInstrument").click( (e) => play(instrumentPlayer, instrument_seq));
+
+$("#btnDownload").click(function(){
+  output_midi = mm.sequenceProtoToMidi(combined_seq); // produces a byteArray
+  output_file = new Blob([output_midi], {type: 'audio/midi'});
+
+  if (window.navigator.msSaveOrOpenBlob) {
+    window.navigator.msSaveOrOpenBlob(file, 'output.mid');
+  } else { // Others
+    const a = document.createElement('a');
+    const url = URL.createObjectURL(file);
+    a.href = url;
+    a.download = 'interp.mid';
+    document.body.appendChild(a);
+    a.click();
+    setTimeout(() => {
+      document.body.removeChild(a);
+      window.URL.revokeObjectURL(url);
+    }, 0);
+  }
+});
 
 
 function play(player, n_seq){
