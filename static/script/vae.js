4b5ac1707b608bbf56b4b1e0152b8093cf0afdb5
jgoffin <jacobgoffin@gmail.com>
2020-05-09 11:21:45 -0700

    added trim, instrument selection wireframe for demo

--- static/script/vae.js
+++ static/script/vae.js
@@ -1,19 +1,27 @@
 // Initialize the model.
 const music_vae = new mm.MusicVAE('https://storage.googleapis.com/magentadata/js/checkpoints/music_vae/mel_4bar_small_q2');
-music_vae.initialize().then(() => {
-  document.getElementById('loading').style.display = 'none';
-  [...document.getElementsByTagName('button')].forEach(b => b.style = '');
-});
+
 
 // Create a player to play the sampled sequence.
+let vaeViz;
 const vaePlayer = new mm.SoundFontPlayer('https://storage.googleapis.com/magentadata/js/soundfonts/sgm_plus');
 const vae_temperature = 1.5
+
+const config = {
+    noteHeight: 6,
+    pixelsPerTimeStep: 60,  // like a note width
+    noteSpacing: 1,
+    noteRGB:'223,66,98',
+    activeNoteRGB:'157, 229, 184',
+
+}
 
 let upload1_quant;
 let upload2_quant;
 let note_seq1;
 let note_seq2;
-let concatenated
+let concatenated;
+let unquantized;
 
 //download
 let output_midi;
@@ -22,32 +30,53 @@ let output_file;
 let output_filename;
 let json_data;
 
-function interpolate(midi1, midi2) {
-  
-  //Quantize note sequences and pass note sequences to interpolate function of MusicVAE
-  mm.urlToNoteSequence(midi1)
-                  .then(ns_val1 => note_seq1 = ns_val1)
-                  .then(vars => upload1_quant = mm.sequences.quantizeNoteSequence(note_seq1, 4))
-                  .then(vars => mm.urlToNoteSequence(midi2))
-                  .then(ns_val2 => note_seq2 = ns_val2)
-                  .then(vars => upload2_quant = mm.sequences.quantizeNoteSequence(note_seq2, 4))
-                  .then(vars => music_vae
-                      .interpolate([upload1_quant, upload2_quant], 4)
-                      .then((sample) => {
-                        concatenated = mm.sequences.concatenate(sample);
-                        concatenated['tempos'][0]['qpm'] = 100;
-                        concatenated['tempos'][0]['time'] = 0;
-                        vaePlayer.start(concatenated);
-                        $("#saveDiv").show();
-                      }));
+init();
+
+$(".noUi-touch-area").click(function(){
+    console.log('HI')
+});
+
+function init(midi1, midi2) {
+    vaePlayer.callbackObject = {
+        run: (note) => vaeViz.redraw(note, true),
+        stop: () => {}
+    };
+
+    music_vae.initialize()
+    .then(function(){
+        mm.urlToNoteSequence(midi1)
+        .then(ns_val1 => note_seq1 = ns_val1)
+        .then(vars => upload1_quant = mm.sequences.quantizeNoteSequence(note_seq1, 4))
+        .then(vars => mm.urlToNoteSequence(midi2))
+        .then(ns_val2 => note_seq2 = ns_val2)
+        .then(vars => upload2_quant = mm.sequences.quantizeNoteSequence(note_seq2, 4))
+        .then(vars => music_vae
+          .interpolate([upload1_quant, upload2_quant], 4)
+          .then((sample) => {
+            concatenated = mm.sequences.concatenate(sample);
+            concatenated['tempos'][0]['qpm'] = 80;
+            concatenated['tempos'][0]['time'] = 0;
+            unquantized = mm.sequences.unquantizeSequence(concatenated);
+            document.getElementById('loading').style.display = 'none';
+            vaeViz = new mm.PianoRollSVGVisualizer(
+                            unquantized,
+                            document.getElementById('vaeViz'),
+                            config
+                            );
+            $("#slider").css("width", "2880px");
+            $("#sample").show();
+            $("#play").show();
+            $("#saveDiv").show();
+            }));
+  });
 }
 
-function stop(){
+function start_stop(){
   if (vaePlayer.isPlaying()) {
     vaePlayer.stop();
   }
   else{
-    vaePlayer.start();
+    vaePlayer.start(concatenated);
   }
 }
 
@@ -95,4 +124,23 @@ function download(){
       window.URL.revokeObjectURL(url);
     }, 0);
   }
+}
+
+
+function modifyInstrument(obj){
+    var program = parseInt(obj.options[obj.selectedIndex].value);
+    concatenated['notes'].forEach(note => note['program']=program);
+}
+
+function trim(){
+    concatenated = mm.sequences.trim(concatenated,64,128);
+    unquantized = mm.sequences.unquantizeSequence(concatenated);
+    vaeViz = new mm.PianoRollSVGVisualizer(
+                            unquantized,
+                            document.getElementById('vaeViz'),
+                            config
+                            );
+    $("#slider").css("width", "800px");
+    $("#vaeViz").css("width", "800px");
+    $("#slider").css("margin-left","220px");
 }
